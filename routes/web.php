78b38bcf4dfc2e8899e46aaa294ced3cddd0f383
tiarainalasania <tiarafeb39@gmail.com>
2022-05-25 08:34:07 +0700

    25/05/2022

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/yusita', function () {
-    return view('yusita');
+Route::get('/tiara', function () {
+    return view('tiara');
 });
